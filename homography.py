11c34fef39dff6ce93021478b6fcbf0c5f073079
Puvvada123 <113325192+Puvvada123@users.noreply.github.com>
2023-05-21 10:57:04 +0530

    Update homography.py

--- homography.py
+++ homography.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
 if __name__ == '__main__' :
-    im_src = cv2.imread("C:/Users/Dama Prasoona/Downloads/harrypotter.png")
+    im_src = cv2.imread("D:\computer vision\naruto.jpg")
     pts_src = np.array([[141, 131], [480, 159], [493, 630],[64, 601]])
-    im_dst = cv2.imread("C:/Users/Dama Prasoona/Downloads/harrypotter.png")
+    im_dst = cv2.imread("D:\computer vision\naruto.jpg")
     pts_dst = np.array([[318, 256],[534, 372],[316, 670],[73, 473]])
     h, status = cv2.findHomography(pts_src, pts_dst)
     im_out = cv2.warpPerspective(im_src, h, (im_dst.shape[1],im_dst.shape[0]))
